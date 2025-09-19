caeb71c92ab900857a0bec99f3befb7dca2f0be9
ebott05 <eboctober2005@gmail.com>
2025-09-19 14:20:20 -0500

    commit with new funciton

---
+++ R/mynotch.R
@@ -0,0 +1,28 @@
+#' Notched Boxplot
+#'
+#' @param data a data table
+#' @param var the variable in question
+#' @param xlab x label, defaults to column name
+#' @param main title of the plot
+#' @param col purple
+#'
+#' @returns a notched boxplot
+#' @export
+#'
+#' @examples
+#' my_notchplot(BHDiameter, spruce)
+my_notchplot <- function(data, var, xlab = NULL, main = NULL, col = "magenta") {
+  var_name <- deparse(substitute(var))
+  x <- data[[var_name]]
+  if (!is.numeric(x)) stop("Selected column must be numeric.")
+
+  if (is.null(xlab)) xlab <- var_name
+  if (is.null(main)) main <- paste("Box and Whisker Plot of", var_name)
+
+  boxplot(x,
+          horizontal = TRUE,
+          notch = TRUE,
+          col = col,
+          xlab = xlab,
+          main = main)
+}
