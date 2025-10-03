6d9d89e084fb21b9c6e2ccc149bbdc5e9f3dc41a
ebott05 <eboctober2005@gmail.com>
2025-10-03 14:30:25 -0500

    second marbles commit

--- R/marbles.R
+++ R/marbles.R
@@ -1,4 +1,15 @@
 #make a function that allows you to make as many iterations of marble pulling
+#' Marble sampling iterations
+#'
+#' @param iter #iterations
+#' @param size #n
+#' @param white #12 marbles
+#' @param black #8 marbles
+#'
+#' @returns #the iterations of that sample
+#' @export
+#'
+#' @examples #sample_marbles(iter=4)
 sample_marbles <- function(iter = 1, size = 5, white = 12, black = 8) {
   # Create the marble vector
   marbles <- c(rep(1, white), rep(0, black))
@@ -13,4 +24,4 @@ sample_marbles <- function(iter = 1, size = 5, white = 12, black = 8) {
   return(results)
 }
 
-sample_marbles(iter= 4)
+#sample_marbles(iter= 4)
