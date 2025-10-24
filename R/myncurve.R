1ca72a9c64ebc2cd63df188c2a38747948968fdc
ebott05 <eboctober2005@gmail.com>
2025-10-24 01:50:14 -0500

    I like 100% forgot to commit this

---
+++ R/myncurve.R
@@ -0,0 +1,27 @@
+#create the function to make a normal distribution curve
+#' Title
+#'
+#' @param mu a vector
+#' @param sigma a vector
+#' @param a av vector
+#'
+#' @returns a list and plot given mu, sigma, and a and generates probabilites off of a
+#' @export
+#'
+#' @examples
+myncurve = function(mu, sigma, a) {
+  # Plot the curve
+  curve(dnorm(x, mean=mu, sd=sigma), xlim = c(mu - 3*sigma, mu + 3*sigma), col= "purple"
+        , ylab = "Density", main = paste("Normal Curve with mu =", mu, "and sigma =", sigma))
+
+  # find and fill in the probability of x =< a
+  xcurve = seq(mu - 3*sigma, a, length=1000)
+  ycurve = dnorm(xcurve, mean=mu, sd=sigma)
+  polygon(c(mu - 3*sigma, xcurve, a), c(0, ycurve, 0), col="purple")
+
+  # Compute probability
+  prob = round(pnorm(a, mean=mu, sd=sigma), 4)
+
+  # Return a named list
+  list(mu = mu, sigma = sigma, a = a, probability = prob)
+}
